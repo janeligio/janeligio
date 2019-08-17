310eb70d8e88ce02611e0b83ecd2afa6104a7d49
janeligio <jeligio808@gmail.com>
2019-08-17 12:52:03 -1000

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import { CSSTransition } from "react-transition-group";
+import _ from 'lodash';
+
 import Makapuu from './public/makapuu.jpg';
 import Headshot from './public/janheadshot.jpg';
 import Tastetea from './public/tastetea.JPG';
@@ -24,7 +26,12 @@ import hdcc2 from './hdccproject_gifs/network.gif';
 import hdcc3 from './hdccproject_gifs/create.gif';
 import hdcc4 from './hdccproject_gifs/all sites printing.gif';
 
-import { FaGithub } from 'react-icons/fa';
+import { 
+	FaGithub, 
+	FaStar, 
+	FaStarHalfAlt, 
+	FaLinkedin,
+	FaInstagram } from 'react-icons/fa';
 import {MdExpandMore, MdExpandLess} from 'react-icons/md';
 
 const border = {
@@ -44,15 +51,20 @@ function App() {
 	};
 	return (
     <div className="App">
-      <header style={{height:'100vh'}} className="App-header grid">
+      <header style={{height:'100%'}} className="App-header grid">
 	      <h1 style={{fontSize:"3em"}} id="myname">
 	      {`<jan.io>`}
 	      </h1>
-	      <div style={{display:'flex', padding:'8em'}}>
+	      <div style={{display:'flex', paddingTop:'3em'}}>
 	      	  <div style={{flex:1}}>
 			      <img className="headshot" style={{height:150, width:150}} src={Headshot} alt="headshot"/>
 		      </div>
 		      <p style={{flex:1}}>Aspiring web developer.</p>
+		  </div>
+		  <div style={{display:'flex', justifyContent:'center', paddingBottom:'3em'}}>
+			<Link link="https://github.com/janeligio" title={<FaGithub size="2em"/>}/> 
+			<Link link="https://www.linkedin.com/in/jan-iverson-eligio-4a7756146/" title={<FaLinkedin size="2em" color="#0077B5"/>}/> 
+			<Link link="https://www.instagram.com/_jan_da_man_/" title={<FaInstagram size="2em" color="#c32aa3"/>}/>
 		  </div>
       </header>
 
@@ -60,39 +72,59 @@ function App() {
 		  
 	  <h2 className="content-title" style={{color:'white', background:'#9299FB'}}>Favorite Pictures</h2>
 
-      	<div id="favorite-pics" className="grid" style={{background:''}}>
-			<div style={{margin:'1em',}}>
-				<div className="favorites-pics-container" style={{display:'flex',flexWrap:'no-wrap'}}>
+				<div className="favorites-pics-container" style={{
+					display:'flex',
+					justifyContent:'space-around', 
+					margin:'1em'
+				}}>
 				<FavoritePicture picture={Makapuu} alt="makapuu" content={<p>hello</p>}/>
 				<FavoritePicture picture={Smoking} alt="makapuu" content={<p>hello</p>}/>
 				<FavoritePicture picture={Pali} alt="makapuu" content={<p>hello</p>}/>
 				</div>
-				<div className="favorites-pics-container" style={{display:'flex'}}>
+				<div className="favorites-pics-container" style={{
+					display:'flex',
+					justifyContent:'space-around',
+					margin:'1em'
+				}}>
 				<FavoritePicture picture={MakapuuHike} alt="makapuu" content={<p>hello</p>}/>
 				<FavoritePicture picture={Waikiki} alt="makapuu" content={<p>hello</p>}/>
 				<FavoritePicture picture={Charlie} alt="makapuu" content={<p>hello</p>}/>
 				</div>
-				<div className="favorites-pics-container" style={{display:'flex'}}>
+				<div className="favorites-pics-container" style={{
+					display:'flex',
+					
+					justifyContent:'space-around',
+					margin:'1em'
+				}}>
 				<FavoritePicture picture={CrouchingLion} alt="makapuu" content={<p>hello</p>}/>
 				<FavoritePicture picture={Elither_1} alt="makapuu" content={<p>hello</p>}/>
 				</div>
-				<div className="favorites-pics-container" style={{display:'flex'}}>
+				<div className="favorites-pics-container" style={{
+					display:'flex',
+					justifyContent:'space-around',
+					margin:'1em'
+				}}>
 				<FavoritePicture picture={Elither_2} alt="makapuu" content={<p>hello</p>}/>
 				<FavoritePicture picture={Elither_3} alt="makapuu" content={<p>hello</p>}/>
 				<FavoritePicture picture={Ralph} alt="makapuu" content={<p>hello</p>}/>
 				</div>
-				<div className="favorites-pics-container" style={{display:'flex'}}>
+				<div className="favorites-pics-container" style={{
+					display:'flex',
+					justifyContent:'space-around',
+					margin:'1em'
+				}}>
 				<FavoritePicture picture={Karaoke} alt="makapuu" content={<p>hello</p>}/>
 				<FavoritePicture picture={Tastetea} alt="makapuu" content={<p>hello</p>}/>
 				</div>
-				<div className="favorites-pics-container" style={{display:'flex'}}>
+				<div className="favorites-pics-container" style={{
+					display:'flex',
+					justifyContent:'space-around',
+					margin:'1em'
+				}}>
 				<FavoritePicture picture={Momi} alt="makapuu" content={<p>hello</p>}/>
 				<FavoritePicture picture={PattyMills} alt="makapuu" content={<p>hello</p>}/>
 				<FavoritePicture picture={Hairspray} alt="makapuu" content={<p>hello</p>}/>
 				</div>
-			</div>
-	   	   </div>
-
 	   	<Section id="projects" title="Projects"/>
 		<div style={{
 			display:'flex',
@@ -166,6 +198,48 @@ function App() {
 			:   <MdExpandLess size='2em' onClick={() => setShowShooots(!showShooots)}/>
 			}		</div>
 	   	<Section id="resume" title="Resume"/>
+		<div style={{
+			padding:'2em'
+		}}>
+			<div style={{
+				display:'flex',
+				flexDirection:'column',
+				margin:'1em 0'
+			}}>
+				<h3 style={{padding:0}}>Jan Iverson Eligio</h3>
+				<p style={{padding:0}}>Honolulu, HI</p>
+				<p style={{padding:0}}>jeligio808@gmail.com</p>
+			</div>
+			<div style={{
+				margin:'1em 0'
+			}}>
+				<h4 style={{margin:0}}>Education</h4>
+				<p style={{padding:0}}>University of Hawaii at Manoa, Spring 2020</p>
+			</div>
+			<div style={{
+				margin:'1em 0'
+			}}>
+				<h4>Skills</h4>
+				<ul style={{listStyleType:'none'}}>
+				<li>JavaScript/ES6 <Rating rating={4}/></li>
+				<li>React <Rating rating={4}/></li>
+				<li>HTML <Rating rating={3}/></li>
+				<li>CSS <Rating rating={3}/></li>
+				<li>Git <Rating rating={2}/></li>
+				<li>C <Rating rating={3}/></li>
+				<li>C++ <Rating rating={2}/></li>
+				</ul>
+			</div>
+			<div style={{
+				margin:'1em 0'
+			}}>
+				<h4>Experience</h4>
+				<h5>Taste Tea</h5>
+				<h5>Web App developer</h5>
+				<h5>Hawaiian Dreding Construction Co.</h5>
+			</div>
+		</div>
+
 	   	<Section id="interests" title="Interests"/>
 	   	<Section id="favorite-movies" title="Favorite Movies"/>
     </main>
@@ -182,6 +256,21 @@ const Section = ({id, title, content}) => (
       	</div>
 );
 
+const Rating = ({rating}) => {
+	const isFloat = rating % 1 !== 0;
+	let elements = []
+	_.times(Math.trunc(rating), 
+		() => elements = [...elements, <FaStar size="1em"/>]);
+	return (
+		<>
+		{
+			elements.map(el => el)
+		}
+		{ isFloat && <FaStarHalfAlt size="1em"/>}
+		</>
+	)
+}
+
 // const FavoritePicture = ({picture, alt, content}) => {
 // 	return (
 // 		<div className="card-picture" style={{margin:'1em'}} id="makapuu-pic">
@@ -191,8 +280,7 @@ const Section = ({id, title, content}) => (
 // };
 const FavoritePicture = ({picture, alt, content}) => {
 	return (
-		<img className="card-picture"
-		 style={{borderRadius:'5%', margin: '1em'}} src={picture} alt={alt} />
+		<img className="card-picture" src={picture} alt={alt} />
 	);
 };
 
