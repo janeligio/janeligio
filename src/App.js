44766e5128a415b4106f76051e0a95010963cfa5
Jan <jeligio808@gmail.com>
2021-01-03 19:16:07 -1000

    Finish basic layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Container, Row, Col } from 'reactstrap';
 
 /* Sections */
 import Header from './components/Header';
@@ -10,9 +11,9 @@ import Interests from './components/Interests';
 
 import Headshot from './public/janheadshot.jpg';
 
-const border = {border:'1px solid black'};
 
 function App() {
+	let projects = [1,2,3,4,5,6,7,8];
 	return (
 		<div className="App">
 			<header>
@@ -21,16 +22,50 @@ function App() {
 					<img className="headshot" src={Headshot} alt="Jan Iverson Eligio's headshot."/>
 					<div className="header-content-text">
 						<p>2020 B.S. Computer Science graduate from UH Manoa. Aspiring software engineer.</p>
-						<a className="link" href="https://github.com/janeligio">GitHub</a>
+						<a className="link-github" href="https://github.com/janeligio">GitHub</a>
 					</div>
 				</div>
 			</header>
+
+			<nav>
+				<div className="tabs">
+					<Tab name="Projects"/>
+					<Tab name="Skills"/>
+					<Tab name="Experience"/>
+					<Tab name="Contact"/>
+				</div>
+			</nav>
+
 			<main className="main">
-				<div style={border}>Tabs</div>
-				<div style={border}>Content</div>
+				<div className="main-content-wrapper">
+					<div className="main-content">
+						<Container fluid>
+							<Row>
+								{projects.map(p => {
+									return (<ProjectCard val={p}/>);
+								})}
+							</Row>
+						</Container>
+					</div>
+				</div>
 			</main>
 		</div>
 	);
 }
 
+const Tab = props => {
+	return (
+		<button className="tab">
+			{props.name}
+		</button>
+	);
+};
+
+const ProjectCard = props => {
+	return (
+		<Col sm={4}>
+			<h1>{props.val}</h1>
+		</Col>
+	);
+}
 export default App;
