a9e0f9d6fa7056a85e963c540d0e211a2dea4cb3
Jan <jeligio808@gmail.com>
2021-01-04 13:35:00 -1000

    Update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Header from './Views/Header/Header.js';
@@ -6,11 +6,12 @@ import Navigation from './Views/Navigation/Navigation.js';
 import Main from './Views/Main/Main.js';
 
 function App() {
+	const [ currentTab, setCurrentTab ] = useState('Projects');
 	return (
 		<div className="App">
 			<Header/>
-			<Navigation/>
-			<Main/>
+			<Navigation currentTab={currentTab} setCurrentTab={setCurrentTab}/>
+			<Main currentTab={currentTab}/>
 		</div>
 	);
 }
--- src/Views/Navigation/Navigation.js
+++ src/Views/Navigation/Navigation.js
@@ -1,25 +1,30 @@
 import React from 'react'
 import './Navigation.css';
 
-const Navigation = () => {
+const Navigation = props => {
+
+    const tabNames = ['Projects', 'Skills', 'Experience', 'Contact'];
+    const { currentTab, setCurrentTab } = props;
+    
     return (
-			<nav>
-				<div className="tabs">
-					<Tab name="Projects"/>
-					<Tab name="Skills"/>
-					<Tab name="Experience"/>
-					<Tab name="Contact"/>
-				</div>
-			</nav>
+      <nav>
+        <div className="tabs">
+          {
+            tabNames.map(t => <Tab name={t} currentTab={currentTab} changeTab={setCurrentTab}/>)
+          }
+        </div>
+      </nav>
     );
 }
 
 const Tab = props => {
-	return (
-		<button className="tab">
-			{props.name}
-		</button>
-	);
+  const { name, currentTab, changeTab } = props;
+  let active = currentTab === name;
+  return (
+    <button className={`tab ${active ? 'active' : ''}`} onClick={() => changeTab(name)}>
+      {name}
+    </button>
+  );
 };
 
 export default Navigation;
